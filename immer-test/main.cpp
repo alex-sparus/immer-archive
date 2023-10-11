c7a4518d3f71c1c7eb3d0af81d75f96dcaf8b0a5
Alex Shabalin <alex.shabalin@pm.me>
2023-10-11 16:30:06 +0200

    Initial Commit

---
+++ immer-test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  immer-test
+//
+//  Created by Alex Shabalin on 11/10/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
