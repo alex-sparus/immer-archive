e1719ca316af5b05a7e3d340433f90f7ff873cee
Alex Shabalin <alex.shabalin@pm.me>
2023-10-13 16:14:35 +0200

    minimal stuff

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    flake-compat,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      clang-format = pkgs.runCommand "clang-format" {} ''
+        mkdir -p $out/bin
+        ln -s ${pkgs.llvmPackages_16.clang-unwrapped}/bin/clang-format $out/bin/
+      '';
+    in rec {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [clang-format cmake ninja];
+      };
+    });
+}
--- immer-test/main.cpp
+++ immer-test/main.cpp
@@ -7,8 +7,27 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+#include <immer/algorithm.hpp>
+#include <immer/vector.hpp>
+
+namespace {
+
+void qwe()
+{
+    // immer::for_each(immer::vector<int>{1}, [](){});
+}
+
+} // namespace
+
+int main(int argc, const char* argv[])
+{
     // insert code here...
     std::cout << "Hello, World!\n";
+
+    auto vec = immer::vector<int>{1};
+    auto v2  = vec.push_back(2);
+
+    std::cout << v2.size() << '\n';
+
     return 0;
 }
